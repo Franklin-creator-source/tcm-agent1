6289be3734a7809796565fba71ed43f7f10ad8a3
Franklin-creator-source <1402660900@qq.com>
2026-07-13 08:49:33 +0800

    Update server.py

--- server.py
+++ server.py
@@ -13,7 +13,7 @@ import urllib.request
 IMA_CLIENT_ID = os.environ.get("IMA_OPENAPI_CLIENTID", "")
 IMA_API_KEY = os.environ.get("IMA_OPENAPI_APIKEY", "")
 KB_ID = os.environ.get("KB_ID", "SV1LP_ohoX7Fq_Up6P1ssrCAuEKyoyL2hQCqunxxrFk=")
-DEEPSEEK_API_KEY = os.environ.get("DEEPSEEK_API_KEY", "")
+DEEPSEEK_API_KEY = os.environ.get("DEEPSEEK_API_KEY", "sk-1421314d63634de08de8b11ea27d95ec")
 PORT = int(os.environ.get("PORT", "8088"))
 
 IMA_BASE_URL = "https://ima.qq.com"
